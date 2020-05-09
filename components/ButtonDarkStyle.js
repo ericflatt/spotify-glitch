7d4a6aae7accf653aab327559ad4fe5614a09396
Glitch (spotify-collab) <none>
2020-05-09 21:05:05 +0000

    💫💍 Updated with Glitch

---
+++ components/ButtonDarkStyle.js
@@ -0,0 +1,7 @@
+import css from 'styled-jsx/css';
+
+export default css`.btn--dark {
+  background-color: pink;
+  border: 1px solid pink;
+  color: white;
+}`;
---
+++ config/auth.js
@@ -0,0 +1,4 @@
+module.exports = {
+  CLIENT_ID: '16630ea94f6c41bc8ef8329155cfe349',
+  CLIENT_SECRET: '21cbe541fd9b429bb812f53cf6e69d8b'
+};
